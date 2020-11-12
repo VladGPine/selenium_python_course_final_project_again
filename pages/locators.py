7e36df64cff280cd4472191409864fe7d61a8074
VladGPine <shikarno_3000@inbox.ru>
2020-11-12 22:26:35 +0300

    login page Page Object added and locators for it added as well

--- pages/locators.py
+++ pages/locators.py
@@ -3,3 +3,12 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, '#login_link')
+
+
+class LoginPageLocators:
+    LOGIN_FORM_ELEMENT = (By.CSS_SELECTOR, '#login_form')
+    EMAIL_LOGIN_INPUT = (By.CSS_SELECTOR, '#id_login-username')
+    PASSWORD_LOGIN_INPUT = (By.CSS_SELECTOR, '#id_login-password')
+    SUBMIT_LOGIN_BUTTON = (By.CSS_SELECTOR, 'button[name="login_submit"]')
+
+    REGISTER_FORM_ELEMENT = (By.CSS_SELECTOR, '#register_form')
